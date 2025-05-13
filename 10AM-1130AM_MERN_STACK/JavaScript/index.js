68cc4923ff622a4964a6ccee1816f4d02813ecce
Mukesh <mukeshtopcoder@gmail.com>
2025-05-13 11:26:50 +0530

    Java_Script_Introduction

---
+++ 10AM-1130AM_MERN_STACK/JavaScript/index.js
@@ -0,0 +1,49 @@
+/*
+var a = 10
+var b = 20
+var c = a+b
+console.log(c)
+
+----------------------------------------------
+
+Operators:-
+Arithmetic Opertor
++ - * / % 
+var a = 7; 
+var b = 5;
+a+b -> 12
+a-b -> 2
+a*b -> 35
+a/b -> 1.4
+a%b -> 2 (it use to calculate Remainder)
+
+var a = 7;
+var b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(11%4);
+
+--------------------------------------------------
+Relational Operator / Conditional Operator
+> < >= <= == !=    (Boolean Response   true/false)
+a = 7
+b = 5
+a>b   -> true 
+a<b   -> false
+a>=b  -> true
+a<=b  -> false
+a==b  -> false
+a!=b  -> true
+
+var a = 7;var b = 5;
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+
+*/
