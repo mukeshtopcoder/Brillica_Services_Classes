fe40de97649d8f7ae785bb86ca75769ed786000d
Mukesh <mukeshtopcoder@gmail.com>
2025-05-19 10:57:01 +0530

    19th_May

---
+++ 10AM-1130AM_MERN_STACK/JavaScript/19th_May_JS_IF_ELSE_Programming_Questions/programmingQuestions.js
@@ -0,0 +1,33 @@
+/*
+WAP to check if a number is even or odd.
+
+var num = 91;
+if ( num%2 == 0 )
+    console.log("Even");
+else
+    console.log("Odd");
+
+WAP to check eligility for voting checking age is 18 or above.
+
+var age = 15;
+if ( age >= 18 )
+    console.log("Eligible");
+else
+    console.log("Not Eligible");
+
+WAP to check a number is 100-90 -> A , 90-80 -> B , 80-70 -> C , 70-60 -> D , 
+less than 60 is otherwise.
+
+var num = 53;
+if(num >= 90)
+    console.log("A");
+else if(num >= 80)
+    console.log("B");
+else if(num >= 70)
+    console.log("C");
+else if(num >= 60)
+    console.log("D");
+else
+    console.log("Otherwise");
+
+*/
